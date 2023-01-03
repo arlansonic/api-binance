eb0a6cb723cbb9a303bef7234114226f686d8865
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2023-01-03 14:10:51 -0400

    Bot para Binance #2

--- api.js
+++ api.js
@@ -1,5 +1,34 @@
 const axios = require('axios');
 const querystring = require('querystring');
+const crypto = require('crypto');
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.SECRET_KEY;
+const apiUrl = process.env.API_URL;
+
+async function privateCall(path, data = {}, method = 'GET') {
+    const timestamp = Date.now();
+    const signature = crypto.createHmac('sha256', apiSecret)
+        .update(`${querystring.stringify({ ...data, timestamp })}`)
+        .digest('hex')
+
+    const newData = { ...data, timestamp, signature }
+    const qs = `?${querystring.stringify(newData)}`
+
+    try {
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`,
+            headers: { 'X-MBX-APIKEY': apiKey }
+        })
+        return result.data
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function accountInfo() {
+    return privateCall('/v3/account')
+}
 
 async function publicCall(path, data, method = 'GET') {
     try {
@@ -23,4 +52,8 @@ async function depth(symbol = 'BTCBRL', limit = 5) {
     return publicCall('/v3/depth', { symbol, limit })
 }
 
-module.exports = { time, depth }
+async function exchangeInfo() {
+    return publicCall('/v3/exchangeInfo')
+}
+
+module.exports = { time, depth, exchangeInfo, accountInfo }
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const api = require('./api')
+const symbol = process.env.SYMBOL
 
 setInterval(async () => {
-    const result = await api.depth()
-    console.log(`Higest Buy: ${result.bids[0][0]}`)
-    console.log(`Lowest Sell: ${result.asks[0][0]}`)
+    let buy = 0, sell = 0;
 
-    const sell = parseInt(result.asks[0][0])
-    if (sell < 880000) {
+    const result = await api.depth(symbol)
+    if (result.bids && result.bids.length) {
+        console.log(`Higest Buy: ${result.bids[0][0]}`)
+        buy = parseInt(result.bids[0][0])
+    }
+
+    if (result.asks && result.asks.length) {
+        console.log(`Lowest Sell: ${result.asks[0][0]}`)
+        sell = parseInt(result.asks[0][0])
+    }
+
+    if (sell < 700) {
         console.log('Hora de Comprar!!')
-    } else if (sell > 900000) {
+
+        const account = await api.accountInfo()
+        const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1)
+        console.log(`POSIÇÃO DA CARTEIRA:`)
+        console.log(coins)
+
+        console.log(await api.accountInfo())
+    } else if (sell > 1000) {
         console.log('Hora de Vender!!')
     } else {
         console.log('Esperando o mercado se mexer...')
